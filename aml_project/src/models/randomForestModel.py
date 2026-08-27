ed62f04233ebf4069463447b6a713fe05fed8275
Alex Rafael Garcia Andrade <201201074@est.umss.edu>
2026-08-26 23:28:20 -0400

    Añadida la modificación de parámetros del train_rf
    Ya se pueden modificar los parámetros del random forest a entrenar por medio del param.yaml

--- aml_project/src/models/randomForestModel.py
+++ aml_project/src/models/randomForestModel.py
@@ -7,10 +7,27 @@ from src.config import RANDOM_STATE
 
 
 class RandomForestModel(BaseModel):
-    def __init__(self, random_state=RANDOM_STATE):
+    def __init__(
+        self,
+        random_state=RANDOM_STATE,
+        n_estimators=100,
+        max_depth=15,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        max_features="sqrt",
+        class_weight="balanced",
+    ):
         super().__init__("Random Forest")
         self.model = RandomForestClassifier(
-            n_estimators=100, max_depth=15, class_weight='balanced', random_state=random_state, n_jobs=-1)
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            max_features=max_features,
+            class_weight=class_weight,
+            random_state=random_state,
+            n_jobs=-1,
+        )
 
     def fit(self, X_train, y_train):
         self.model.fit(X_train, y_train)
--- aml_project/src/stages/train_rf.py
+++ aml_project/src/stages/train_rf.py
@@ -1,3 +1,4 @@
+import yaml
 import mlflow
 import pandas as pd
 from src import config
@@ -6,13 +7,24 @@ from src.tracking import start_run
 
 
 def main():
+    params = yaml.safe_load(open("params.yaml"))["random_forest"]
+
     X_train = pd.read_csv(config.PROCESSED_DATA_DIR / "X_train_processed.csv")
     y_train = pd.read_csv(config.PROCESSED_DATA_DIR / "y_train.csv").squeeze()
 
     with start_run("train_rf"):
         mlflow.sklearn.autolog(log_models=False)
+        # n_estimators, max_depth, ... desde params.yaml
+        mlflow.log_params(params)
 
-        model = RandomForestModel()
+        model = RandomForestModel(
+            n_estimators=params["n_estimators"],
+            max_depth=params["max_depth"],
+            min_samples_split=params["min_samples_split"],
+            min_samples_leaf=params["min_samples_leaf"],
+            max_features=params["max_features"],
+            class_weight=params["class_weight"],
+        )
         model.fit(X_train, y_train)
         model.save(config.MODELS_DIR / "rf_model.pkl")
 
